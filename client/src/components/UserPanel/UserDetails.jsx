c8be32ee6b7d26f3b0639521f141079431214efc
brewchetta <chett.tiller@gmail.com>
2024-03-21 15:32:24 -0400

    client now in vite

--- client/src/components/UserPanel/UserDetails.jsx
+++ client/src/components/UserPanel/UserDetails.jsx
@@ -1,12 +1,13 @@
 function UserDetails({currentUser, logout}) {
 
-  return (
-    <div className='user-details'>
-      <h2>Welcome {currentUser.username}!</h2>
-      <button onClick={logout}>Logout</button>
-    </div>
-  )
-
-}
-
-export default UserDetails
+    return (
+      <div className='user-details'>
+        <h2>Welcome {currentUser.username}!</h2>
+        <button onClick={logout}>Logout</button>
+      </div>
+    )
+  
+  }
+  
+  export default UserDetails
+  
